88807955e45df18ffc93ec1828e2b76ad8aa622c
Keshav Bihani <keshavbihani22@gmail.com>
2020-04-20 16:16:45 +0530

    added postgres in production.py

--- myplaylist/settings/production.py
+++ myplaylist/settings/production.py
@@ -6,13 +6,18 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
+
+DATABASES = {
+		'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'myplaylist_rds',
+		'USER': 'postgres',
+		'PASSWORD': 'postgres',
+		'HOST': 'myplaylist-rds.cuyk4lxvhrqe.ap-south-1.rds.amazonaws.com',
+		'PORT': '5432',
+		}
+}
 
 # BROKER_URL = 'amqp://localhost'
 # BROKER_HEARTBEAT = 40
